a92247de672f2bdf4a50a4d76cab0834af4cfea6
Felipe Santos <santos.flip@gmail.com>
2024-01-06 20:55:38 -0300

    gerador a partir do processor

--- companion/backend.py
+++ companion/backend.py
@@ -37,9 +37,117 @@ PYDANTIC_EXAMPLES_PROMPT = PromptTemplate.from_template(
 Answer with the code only"""
 )
 
+PROCESSOR_PROMPT =  PromptTemplate.from_template(
+    """Create an python class to perform {comments}. Follow the examples
+
+Here is the base class definition:
+from pyspark.sql import SparkSession
+import pyspark.sql.types as t
+import engine.models as m
+
+class BaseProcessor():
+
+    def equals_datatype(self, one: m.DataType, two: t.StructType):
+        return self.to_pyspark_datatype(one) != two.dataType
+
+    def to_pyspark_datatype(self, datatype: m.DataType) -> t.DataType:
+        match (datatype):
+            case m.DataType.Date:
+                return t.DateType()
+            case m.DataType.Numeric:
+                return t.DecimalType(25,7)
+            case m.DataType.Text:
+                return t.StringType()
+            case _:
+                return t.StringType()
+
+Here is a example of a sort processor:
+from pyspark.sql import SparkSession, DataFrame
+from engine.processors.base import BaseProcessor
+from engine.models import *
+
+class SortStepProcessor(BaseProcessor):
+    def __init__(
+        self,
+        spark: SparkSession,
+        model: SortStep,
+        input_df: DataFrame,
+    ) -> None:
+        self.spark = spark
+        self.model = model
+        self.model.clauses.sort(key=lambda x: x.id)
+        self.input_df = input_df
+
+    def run(self) -> list[StepResult]:
+        self.__validate()
+        df = self.input_df
+        cols = []
+        for clause in self.model.clauses:
+            column = df[clause.inputColumnName]
+            if clause.descending:
+                column = column.desc()
+            cols.append(column)
+        df = df.orderBy(cols)            
+        return [StepResult(port=self.model.outputPorts[0], dataframe=df)]
+
+    def __validate(self):
+        for clause in self.model.clauses:
+            if not clause.isValid:
+                raise ValueError("isValid has to be true") 
+            if clause.inputColumnName not in self.input_df.columns:
+                raise ValueError(f"column {{clause.inputColumnName}} not found")
+
+The models for the inputs and outputs:
+class StepResult(EngineModel):
+    port: OutputPort
+    dataframe: Any
+
+class OutputPort(EngineModel):
+    symbol: PortSymbol
+    code: Optional[str] = None
+    columns: list[PortColumn]
+
+class PortColumn(EngineModel):
+    name: str
+    dataType: DataType
+    columnIndex: int
+
+class DataType(Enum):
+    Text = "Text"
+    Numeric = "Numeric"
+    Date = "Date"
+
+
+class SummarizeAction(Enum):
+    GroupBy = "GroupBy"
+    Sum = "Sum"
+    Count = "Count"
+    CountDistinct = "CountDistinct"
+    Min = "Min"
+    Max = "Max"
+    Average = "Average"
+    First = "First"
+    Last = "Last"
+
+class SummarizeStepClause(EngineModel):
+    isValid: bool
+    isActive: bool
+    inputColumnName: str
+    outputColumnName: str
+    dataType: DataType
+    action: SummarizeAction
+
+
+class SummarizeStep(BaseStep):
+    componentId: Literal["Summarize"]
+    clauses: list[SummarizeStepClause]
+
+Answer with the code only."""
+)
+
 
 def run_qa(query: str, filter: list[str] = None) -> Any:
-    chat = ChatOpenAI(verbose=True, temperature=0)
+    chat = ChatOpenAI(verbose=True, temperature=0, model="gpt-4")
 
     search_kwargs = {}
     if filter and len(filter) > 0:
@@ -68,10 +176,18 @@ def run_pydantic_json_convert(json_obj: str, comments: str = ""):
 
 
 def run_pydantic_create_examples(pydantic_models: str, comments: str = ""):
-    llm = ChatOpenAI(verbose=True, temperature=0.2)
+    llm = ChatOpenAI(verbose=True, temperature=0, model="gpt-4")
     chain = LLMChain(llm=llm, prompt=PYDANTIC_EXAMPLES_PROMPT)
     return chain.run(
         pydantic_models=pydantic_models,
+        comments=comments,
+    )
+
+def run_create_processor(comments: str = ""):
+    # eu sou exatamente o que meu pai fazia comigo, jogava na tv e dava comida processada
+    llm = ChatOpenAI(verbose=True, temperature=0, model="gpt-4")
+    chain = LLMChain(llm=llm, prompt=PROCESSOR_PROMPT)
+    return chain.run(
         comments=comments,
     )
 
@@ -190,10 +306,13 @@ if __name__ == "__main__":
 # """, "create a nutella ice cream example")
 #     print(result)
 
-    result = run_pydantic_create_examples("""class User(BaseModel):
-    id: int
-    name: str = 'John Doe'
-    signup_ts: Optional[datetime] = None
-
-""", "create a famous person example")
+    result = run_create_processor("summarization of a dataframe from the SummarizeStep model")
     print(result)
+
+#     result = run_pydantic_create_examples("""class User(BaseModel):
+#     id: int
+#     name: str = 'John Doe'
+#     signup_ts: Optional[datetime] = None
+
+# """, "create a famous person example")
+#     print(result)
